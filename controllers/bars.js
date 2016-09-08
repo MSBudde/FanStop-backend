3c916bb62b36f59ce1a4e4529622b2eb00e3b90d
Michael Budde <MICHAEL.s.budde@gmail.com>
2016-09-07 17:40:13 -0700

    bar info

--- controllers/bars.js
+++ controllers/bars.js
@@ -10,26 +10,40 @@ function index(req, res, next) {
 
 
 function create(req, res, next) {
-  Bar
-  .create(req.body)
-  .then(function(bar) {
-    res.json({
-      success: true,
-      message: 'Successfully created bar.',
-      data: {
-        name: bar.name,
-        address: bar.address,
-        id: bar._id
-      }
-    });
-  }).catch(function(err) {
-    if (err.message.match(/E11000/)) {
-      err.status = 409;
+  var newBar = {
+    name: req.body.name,
+    address: req.body.display_address.address[0],
+    lat: req.body.display_address.coordinate.lattitude,
+    lng: req.body.display_address.coordinate.longitude
+  }
+  Bar.findOne({name: newBar.name}, function(err, storedBar) {
+    if(storedBar){
+      res.json({message: 'bar already created'})
+
     } else {
-      err.status = 422;
+      Bar
+      .create(newBar)
+      .then(function(bar) {
+        res.json({
+          success: true,
+          message: 'Successfully created bar.',
+          data: {
+            name: bar.name,
+            address: bar.postal_code,
+            id: bar._id
+          }
+        });
+      }).catch(function(err) {
+        if (err.message.match(/E11000/)) {
+          err.status = 409;
+        } else {
+          err.status = 422;
+        }
+        next(err);
+      });
+
     }
-    next(err);
-  });
+  } )
 };
 
 function currentBar(req, res, next) {
@@ -49,7 +63,7 @@ function currentBar(req, res, next) {
 
 
   function findOrCreate(data, cb) {
-    Bar.findOne({name: data.name}, function(err, result){
+    Bar.findOne({name: data.x`name}, function(err, result){
       if(!result){
         bar.name = data.name;
         bar.save(cb)
